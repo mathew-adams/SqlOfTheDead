08cf9eb44bc7aad3eebd094d545b8d8757d3ace2
Mathew Adams <Matadams@gmail.com>
2025-02-11 18:17:29 +0100

    Fix namespaces

--- SqlOfTheDead/SqlOfTheDead.Client/Program.cs
+++ SqlOfTheDead/SqlOfTheDead.Client/Program.cs
@@ -3,20 +3,19 @@ using MudBlazor.Services;
 using SqlOfTheDead.Client.Pages.Table;
 using SqlOfTheDead.Models;
 
-namespace SqlOfTheDead.Client
+namespace SqlOfTheDead.Client;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.Services.AddMudServices();
+        builder.Services.AddScoped(sp => new HttpClient()
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddMudServices();
-            builder.Services.AddScoped(sp => new HttpClient()
-            {
-                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-            });
-            builder.Services.AddScoped<IZombieTable, ServiceTableClient>();
-            await builder.Build().RunAsync();
-        }
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        });
+        builder.Services.AddScoped<IZombieTable, ServiceTableClient>();
+        await builder.Build().RunAsync();
     }
 }
--- SqlOfTheDead/SqlOfTheDead/Program.cs
+++ SqlOfTheDead/SqlOfTheDead/Program.cs
@@ -6,57 +6,56 @@ using SqlOfTheDead.Models;
 using SqlOfTheDead.Routes;
 using SqlOfTheDead.Services;
 
-namespace SqlOfTheDead
+namespace SqlOfTheDead;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add MudBlazor services
+        builder.Services.AddMudServices();
+
+        builder.Services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(builder.Configuration["ZombieTables:ConnectionString"]));
+
+        builder.Services.AddScoped<IZombieTable, ServiceTableServer>();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add MudBlazor services
-            builder.Services.AddMudServices();
-
-            builder.Services.AddDbContext<DatabaseContext>(options =>
-                    options.UseSqlServer(builder.Configuration["ZombieTables:ConnectionString"]));
-
-            builder.Services.AddScoped<IZombieTable, ServiceTableServer>();
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents()
-                .AddInteractiveWebAssemblyComponents();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAntiforgery();
-
-            app.MapStaticAssets();
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode()
-                .AddInteractiveWebAssemblyRenderMode()
-                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
-
-            app.MapGet("api/table", RouteTable.GetTables);
-            app.MapGet("api/table/id", RouteTable.GetTableIds);
-            app.MapPost("api/table", RouteTable.AddTable);
-            app.MapDelete("api/table/{tableId}", RouteTable.DeleteTable);
-
-            app.Run();
+            app.UseWebAssemblyDebugging();
         }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.MapGet("api/table", RouteTable.GetTables);
+        app.MapGet("api/table/id", RouteTable.GetTableIds);
+        app.MapPost("api/table", RouteTable.AddTable);
+        app.MapDelete("api/table/{tableId}", RouteTable.DeleteTable);
+
+        app.Run();
     }
 }
